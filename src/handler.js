b0de011ffa59b685c46a80111a501e3d3e2bbf06
Giulia <teggi.giulia@gmail.com>
2018-03-28 11:59:48 +0100

    create static handler and route Relates #11

--- src/handler.js
+++ src/handler.js
@@ -33,8 +33,39 @@ const handleLatest = (req, res) => {
   })
 } 
 
+const handleStatic = (request, response) => {
+  console.log("handle static reached"); 
+  console.log("Request.url :", request.url)
+
+  const extension = request.url.split(".")[1];  
+  console.log("Extension :", extension); 
+
+  const extensionType = {
+      html: 'text/html',
+      css: 'text/css',
+      js: 'application/js',
+      ico: 'image/x-icon',
+      svg: 'image/svg+xml'
+  }
+
+  const filePath = path.join(__dirname, '..', request.url)
+  console.log("filePath: ", filePath)
+  fs.readFile(filePath, (error, file) => {
+      if (error) {
+          response.writeHead(500, { 'Content-Type': 'text/html' });
+          response.end('<h1>Sorry we can\'t find the static file</h1>');
+      } else {
+          response.writeHead(200, { 'Content-Type':`${extensionType[extension]}` });
+          console.log("Content type:", `${extensionType[extension]}`)
+          response.end(file);
+      }
+  })
+
+}
+
 
 module.exports = { 
   handleHome,
   handleLatest,
+  handleStatic
  }

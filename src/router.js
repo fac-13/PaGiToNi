b89dac02980acb84c05d9b749282b4ddd64c562d
Parissai <parissa.jamali@gmail.com>
2018-03-29 12:46:24 +0100

    change router importing functions Relates #56

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
-const handler = require("./handler");
+const {handleHome, handleLatest, handleStatic, handleSearch} = require("./handler");
 
 const router = (request, response) => {
   const url = request.url;
   if (url === "/") {
-    handler.handleHome(request, response);
+    handleHome(request, response);
   } else if (url === "/latest") {
-    handler.handleLatest(request, response);
+    handleLatest(request, response);
   } else if (url.indexOf("public") !== -1) {
-    handler.handleStatic(request, response);
+    handleStatic(request, response);
   } else if (url.startsWith("/search")) {
-    handler.handleSearch(request, response);
+    handleSearch(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end("Page not found");

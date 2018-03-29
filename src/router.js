f5d840d34c8a030567abf6bd487ceba5ec0500a9
Parissai <parissa.jamali@gmail.com>
2018-03-29 12:50:56 +0100

    format router Relates #56

--- src/router.js
+++ src/router.js
@@ -1,18 +1,23 @@
-const {handleHome, handleLatest, handleStatic, handleSearch} = require("./handler");
+const {
+  handleHome,
+  handleLatest,
+  handleStatic,
+  handleSearch
+} = require('./handler');
 
 const router = (request, response) => {
   const url = request.url;
-  if (url === "/") {
+  if (url === '/') {
     handleHome(request, response);
-  } else if (url === "/latest") {
+  } else if (url === '/latest') {
     handleLatest(request, response);
-  } else if (url.indexOf("public") !== -1) {
+  } else if (url.indexOf('public') !== -1) {
     handleStatic(request, response);
-  } else if (url.startsWith("/search")) {
+  } else if (url.startsWith('/search')) {
     handleSearch(request, response);
   } else {
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.end("Page not found");
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end('Page not found');
   }
 };
 
